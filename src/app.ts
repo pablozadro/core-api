0d646a26fae87a9370674eb4c0c76345bb872d31
Pablo Zadro <pablo.zadro@globallogic.com>
2025-04-09 10:31:07 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import api from '@/api';
+
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/api', api);
+
+
+export default app;
