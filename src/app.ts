8386cdff333390f564919759c8a6d6223fd86996
Pablo Zadro <pablozadro@gmail.com>
2025-04-17 17:29:43 -0300

    Update postman

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 import { errorMiddleware, dbMiddleware } from '@/core/middlewares';
 import healthApi from '@/health/api';
 import authApi from '@/auth/authApi';
@@ -8,6 +9,7 @@ import authApi from '@/auth/authApi';
 
 /* App */
 const app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
